61012588883b5dcc7e81130e9651359a5aa46051
Himal Rai <himal@sbsolutionsnepal.com>
2022-01-20 00:17:52 +0545

    add login page

--- src/app/@theme/material/header/header.component.ts
+++ src/app/@theme/material/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from "@angular/router";
+import {Router, RouterOutlet} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -18,4 +18,5 @@ export class HeaderComponent implements OnInit {
   signInPage() {
     this.router.navigate(['auth/login'])
   }
+
 }
--- src/app/authorizedComponents/auth/auth.component.ts
+++ src/app/authorizedComponents/auth/auth.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {RouterOutlet} from "@angular/router";
+import {slideInAnimation} from "../../@core/animation/animation";
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.scss']
+  styleUrls: ['./auth.component.scss'],
+  animations: [
+    slideInAnimation
+  ]
 })
 export class AuthComponent implements OnInit {
 
@@ -12,4 +17,7 @@ export class AuthComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet?.activatedRouteData?.['animation'];
+  }
 }
